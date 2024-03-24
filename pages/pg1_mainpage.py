8fd5697d77c32ed4f5affa07a759c762db2e48b2
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 16:36:53 -0700

    Update pg1_mainpage.py

--- pages/pg1_mainpage.py
+++ pages/pg1_mainpage.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from requests import get
 import datetime
+import pandas as pd
+import plotly.express as px
+import json
 
 key = st.secrets["API_KEY"]
 st.title("Hello")
@@ -14,3 +17,14 @@ to_date = str(today_date.strftime('%Y-%m-%d'))
 
 json_data = get(f"https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from_date}/{to_date}?apiKey={key}").json()
 st.write(json_data)
+
+df = pd.DataFrame(json_data['results'])
+
+# Convert timestamps to readable dates
+df['date'] = pd.to_datetime(df['t'], unit='ms')
+
+# Plot using Plotly
+fig = px.line(df, x='date', y='c', title=f"Closing Prices Over Time for {json_data['ticker']}")
+
+# Display the chart in Streamlit
+st.plotly_chart(fig)
