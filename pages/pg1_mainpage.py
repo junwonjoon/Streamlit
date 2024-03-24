d34478d60b077dba3b613b8cd2764d751ddadc47
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 16:59:01 -0700

    added a select button

--- pages/pg1_mainpage.py
+++ pages/pg1_mainpage.py
@@ -3,9 +3,15 @@ from requests import get
 import datetime
 
 key = st.secrets["API_KEY"]
-# key_temp = 'c4NLm5QgWNWDBWp530A_Ypnxc6PHmmtD'
 st.title("Hello")
 today_date = datetime.datetime.now()
+genre = st.radio(
+    "What stock price do you want to see?",
+    ["Google", "Apple", "Microsoft", "NVIDIA"])
+
+
+
+st.write("You selected:", genre)
 
 stocksTicker = "AAPL"
 multiplier = 1
@@ -14,7 +20,7 @@ from_date = "2024-01-01"
 to_date = str(today_date.strftime('%Y-%m-%d'))
 
 json_data = get(f"https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from_date}/{to_date}?apiKey={key}").json()
-if json_data["status"] is not "error":
+if json_data["status"] is not "ERROR":
     st.write(json_data["status"])
 else:
     st.write("Error Occured")
