2645ac8911b3215d9e8d078e65e623f3077591c2
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 15:46:23 -0700

    Update pg1_mainpage.py

---
+++ pages/pg1_mainpage.py
@@ -0,0 +1,24 @@
+import streamlit as st
+# import os
+# from polygon import RESTClient
+# from requests import get
+
+# secret_value = os.getenv('MY_SECRET')
+
+# print(secret_value)
+
+st.title("Hello")
+
+# Hello'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-01-09?apiKey=c4NLm5QgWNWDBWp530A_Ypnxc6PHmmtD
+# aggs = []
+# for a in client.list_aggs(
+#     "AAPL",
+#     1,
+#     "minute",
+#     "2022-01-01",
+#     "2023-02-03",
+#     limit=50000,
+# ):
+#     aggs.append(a)
+
+# print(aggs)
