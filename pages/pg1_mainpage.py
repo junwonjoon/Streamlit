faaa9a01aea3504763bdc8524a9f2c2bd19de2a0
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 16:24:57 -0700

    fixed bugs

--- pages/pg1_mainpage.py
+++ pages/pg1_mainpage.py
@@ -10,9 +10,7 @@ stocksTicker = "APPL"
 multiplier = 1
 timespan = "week"
 from_date = "2024-01-01"
-to_date = today_date.strftime('%Y-%m-%d')
+to_date = str(today_date.strftime('%Y-%m-%d'))
 
-json_data = get(
-    f"https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from_date
-}/{to_date}?apiKey={key}").json()
+json_data = get(f"https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from_date}/{to_date}?apiKey={key}").json()
 st.write(json_data)
