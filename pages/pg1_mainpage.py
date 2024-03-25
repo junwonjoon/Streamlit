cba58d4390903d9bb89a6ca11460d46d588f5a27
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 18:27:06 -0700

    it didn't work so updating the st.write function

--- pages/pg1_mainpage.py
+++ pages/pg1_mainpage.py
@@ -54,8 +54,9 @@ if st.button("Continue", type="primary"):
         average_price = [element["vw"] for element in stock_data]
         the_date_miliseconds = [element["t"] for element in stock_data]
         human_readable_dates = [datetime.datetime.fromtimestamp(element / 1000).strftime('%Y-%m-%d %H:%M:%S') for element in the_date_miliseconds]
-        things_to_print = [average_price[i] + human_readable_dates[i] for i in range(json_data["resultsCount"])]
-        st.write(things_to_print)
+        st.write(human_readable_dates)
+        st.write(average_price)
+
 
 
 
