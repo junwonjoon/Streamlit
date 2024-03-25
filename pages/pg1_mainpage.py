1a8a2dff7deaaa5b067c046cc9f22e4f80243990
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 17:52:40 -0700

    added more error handling

--- pages/pg1_mainpage.py
+++ pages/pg1_mainpage.py
@@ -14,19 +14,17 @@ dict_stocksTicker ={"Apple Inc.": "AAPL",
                     "Johnson & Johnson": "JNJ",
                     "Walmart Inc.": "WMT",
                      "Visa Inc.": "V"}
-
 key = st.secrets["API_KEY"]
-today_date = datetime.datetime.now()
 genre = st.radio(
     "What stock price do you want to see?",
     [key for key in dict_stocksTicker.keys()])
 
+st.header()
 start_date_select = st.date_input("When should be the start date?", datetime.date(2024, 1, 1))
 st.write("The start date is", start_date_select)
 
 end_date_select = st.date_input("When should be the end date?", datetime.datetime.now() - datetime.timedelta(days=1))
 st.write("The end date is", end_date_select)
-
 
 timespan_select = st.select_slider(
     'Select the timespan',
@@ -46,10 +44,13 @@ if st.button("Continue", type="primary"):
     to_date = end_date_select
 
     json_data = get(f"https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from_date}/{to_date}?apiKey={key}").json()
-    if json_data["status"] is not "ERROR":
-        st.write(json_data)
+    if json_data["status"] is "ERROR":
+        st.write("Too many request were created, maximum request is 5 per minute, try again a minute later")
+    elif json_data["status"] is "NOT_AUTHORIZED":
+        st.write("Sorry this feature is not availiable to use")
     else:
-        st.write("Error Occured")
+        st.write(json_data)
+
 
 
 
