f79780c0685660e326731732e4f9c2880ae15347
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 18:27:37 -0700

    trying again to debug

--- pages/pg1_mainpage.py
+++ pages/pg1_mainpage.py
@@ -54,7 +54,6 @@ if st.button("Continue", type="primary"):
         average_price = [element["vw"] for element in stock_data]
         the_date_miliseconds = [element["t"] for element in stock_data]
         human_readable_dates = [datetime.datetime.fromtimestamp(element / 1000).strftime('%Y-%m-%d %H:%M:%S') for element in the_date_miliseconds]
-        st.write(human_readable_dates)
         st.write(average_price)
 
 
