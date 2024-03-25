e9ea94453a29a9bdbed73f8ee5266a8d3cbaa6c4
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 19:35:47 -0700

    made the function to generate dictionary

--- pages/pg1_mainpage.py
+++ pages/pg1_mainpage.py
@@ -3,6 +3,32 @@ from requests import get
 import datetime
 import pandas as pd
 
+def generate_stockdictionary(dict_stocksTicker:dict, timespan_multiplier_select:int, timespan_select:str,
+                   start_date_select:datetime, end_date_select:datetime) -> dict:
+    stocksTicker = dict_stocksTicker[stocksTicker_select]
+    multiplier = timespan_multiplier_select
+    timespan = timespan_select
+    from_date = start_date_select
+    to_date = end_date_select
+
+    json_data = get(f"https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from_date}/{to_date}?apiKey={key}").json()
+    if json_data["status"] == "ERROR":
+        st.write("Too many request were created, maximum request is 5 per minute, try again a minute later")
+    elif json_data["status"] == "NOT_AUTHORIZED":
+        st.write("Sorry, the range you have assigned contain too many steps! Please reduce the range of steps by increasing the multiplier or decrease the date difference")
+    else:
+        # st.write(json_data)
+        average_stock_price = [element["vw"] for element in json_data["results"]]
+        the_date_miliseconds = [element["t"] for element in json_data["results"]]
+        human_readable_date = [datetime.datetime.fromtimestamp(element / 1000).strftime('%Y-%m-%d %H:%M:%S') for element in the_date_miliseconds]
+        # st.write(average_stock_price)
+        # st.write(human_readable_date)
+        data = {
+        'Time': pd.to_datetime(human_readable_date),
+        f'Average Stock Price of the {timespan}': average_stock_price
+        }
+        return pd.DataFrame(data)
+
 st.title("Stock Price!")
 dict_stocksTicker ={"Apple Inc.": "AAPL",
                     "Microsoft Corporation": "MSFT",
@@ -44,32 +70,11 @@ timespan_multiplier_select = st.number_input('Enter the timespan multiplier', 1,
 st.write('Timespan multiplier is:', timespan_multiplier_select)
 
 if st.button("Generate Table", type="primary"):
-    stocksTicker = dict_stocksTicker[stocksTicker_select]
-    multiplier = timespan_multiplier_select
-    timespan = timespan_select
-    from_date = start_date_select
-    to_date = end_date_select
-
-    json_data = get(f"https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from_date}/{to_date}?apiKey={key}").json()
-    if json_data["status"] == "ERROR":
-        st.write("Too many request were created, maximum request is 5 per minute, try again a minute later")
-    elif json_data["status"] == "NOT_AUTHORIZED":
-        st.write("Sorry, the range you have assigned contain too many steps! Please reduce the range of steps by increasing the multiplier or decrease the date difference")
-    else:
-        # st.write(json_data)
-        average_stock_price = [element["vw"] for element in json_data["results"]]
-        the_date_miliseconds = [element["t"] for element in json_data["results"]]
-        human_readable_date = [datetime.datetime.fromtimestamp(element / 1000).strftime('%Y-%m-%d %H:%M:%S') for element in the_date_miliseconds]
-        # st.write(average_stock_price)
-        # st.write(human_readable_date)
-        data = {
-        'Time': pd.to_datetime(human_readable_date),
-        f'Average Stock Price of the {timespan}': average_stock_price
-        }
-        df = pd.DataFrame(data)
-        st.table(df)
-        df.set_index('Time', inplace=True)
-        st.line_chart(df, color = '#ef4423')
+    data = generate_stockdictionary(dict_stocksTicker, timespan_multiplier_select, timespan_select,start_date_select, end_date_select)
+    df = pd.DataFrame(data)
+    st.table(df)
+    df.set_index('Time', inplace=True)
+    st.line_chart(df, color = '#ef4423')
 
 
 
